12f32162a756d371f076cebec6c1e7b1b54fcbdd
myongkunshin <myongkunshin@uchicago.edu>
2023-03-27 20:43:53 -0500

    Created class example.

--- class_1_testing.py
+++ class_1_testing.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Mon Mar 27 20:36:36 2023
 
-@author: cclapp
+@author: myongkunshin
 """
 
-print("Hello, World!!!")
+print('Hello, World!!!')
 
-print("No more math.")
+x = 10
+
+print(x*2)
+
+
 
